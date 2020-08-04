55df32079dd56b205e82afc913cfae1af7bbd7c3
Farid Hernandez <farid.hdz.64@gmail.com>
2020-08-04 17:26:50 -0600

    Updates

---
+++ precache-manifest.36cb5399c256d67804b1a209b5d62b39.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c99f48ba31d57fd82ca67ee3d19ff2c3",
+    "url": "/itca-first-example/index.html"
+  },
+  {
+    "revision": "cfa2f39f845e168b9d59",
+    "url": "/itca-first-example/static/css/main.2d9f764c.chunk.css"
+  },
+  {
+    "revision": "3bed243d013bba0138d4",
+    "url": "/itca-first-example/static/js/2.c18c7707.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/itca-first-example/static/js/2.c18c7707.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfa2f39f845e168b9d59",
+    "url": "/itca-first-example/static/js/main.5c212da1.chunk.js"
+  },
+  {
+    "revision": "5da24fb3cf5ace09988f",
+    "url": "/itca-first-example/static/js/runtime-main.2daa78b1.js"
+  }
+]);
