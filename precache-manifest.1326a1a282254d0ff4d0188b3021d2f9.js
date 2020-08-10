f5f71b9fda4409d2ec3cd28581000363c1e08667
Farid Hernandez <farid.hdz.64@gmail.com>
2020-08-09 21:59:30 -0600

    Updates

--- precache-manifest.1326a1a282254d0ff4d0188b3021d2f9.js
+++ precache-manifest.1326a1a282254d0ff4d0188b3021d2f9.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e0a1842d472aec59ed003d67018439c4",
+    "revision": "9d8540ab6c463ec53853ac2981736e2f",
     "url": "/react-itca-webinar-full-example/index.html"
   },
   {
-    "revision": "667d027925c0d5647e36",
+    "revision": "7af3b2d8632f59a001e7",
     "url": "/react-itca-webinar-full-example/static/css/main.b5c59299.chunk.css"
   },
   {
-    "revision": "417d28c92a562ed9d6c3",
-    "url": "/react-itca-webinar-full-example/static/js/2.fb3dbd96.chunk.js"
+    "revision": "ade870a80ce624bcbf37",
+    "url": "/react-itca-webinar-full-example/static/js/2.e8f2dff6.chunk.js"
   },
   {
     "revision": "0749163b59fbee32225059cb60c18af6",
-    "url": "/react-itca-webinar-full-example/static/js/2.fb3dbd96.chunk.js.LICENSE.txt"
+    "url": "/react-itca-webinar-full-example/static/js/2.e8f2dff6.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "667d027925c0d5647e36",
-    "url": "/react-itca-webinar-full-example/static/js/main.1eabf7e1.chunk.js"
+    "revision": "7af3b2d8632f59a001e7",
+    "url": "/react-itca-webinar-full-example/static/js/main.34b0d175.chunk.js"
   },
   {
     "revision": "4303f28f3c16f28bb766",
